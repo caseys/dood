fc0448d48df1c3a5e294221a4c6e4bac1160122a
casey spain <casey@migreat.com>
2015-08-19 00:29:03 -0400

    dood birth

---
+++ index.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use Mojolicious::Lite;
+
+get '/' => sub {
+  my $self = shift;
+  $self->render('index');
+};
+
+app->start;
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'foo';
+foo mc'bar
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
